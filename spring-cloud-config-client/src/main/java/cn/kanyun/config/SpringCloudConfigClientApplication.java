b620f0dd88d190edede5e66c1851599806f9d841
KANYUN <949955482@qq.com>
2019-02-24 19:43:05 +0800

    springcloud 配置中心测试

--- spring-cloud-config-client/src/main/java/cn/kanyun/config/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/cn/kanyun/config/SpringCloudConfigClientApplication.java
@@ -3,7 +3,6 @@ package cn.kanyun.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * SpringCloud配置仓库客户端
@@ -16,14 +15,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
  * 服务发现注解
  */
 @EnableDiscoveryClient
-/**
- * 这个注解是配置刷新的注解,如果配置中心中的信息发生变化的话，客户端的信息是不会跟着变化的
- * 需要增加一个监控模块：spring-boot-starter-actuator 在pom.xml中增加依赖
- * 并且在Controller中增加@RefreshScope注解。
- * actuator给我们提供了一个/refresh接口，修改完git仓库信息之后，向这个接口发送一个POST信息，
- * 就会更新配置信息了。
- */
-@RefreshScope
+
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
--- spring-cloud-config-client/src/main/java/cn/kanyun/config/SpringCloudConfigClientController.java
+++ spring-cloud-config-client/src/main/java/cn/kanyun/config/SpringCloudConfigClientController.java
@@ -1,6 +1,7 @@
 package cn.kanyun.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author KANYUN
  */
 @RestController
+
+/**
+ * 这个注解是配置刷新的注解,如果配置中心中的信息发生变化的话，客户端的信息是不会跟着变化的
+ * 需要增加一个监控模块：spring-boot-starter-actuator 在pom.xml中增加依赖
+ * 并且在Controller中增加@RefreshScope注解。
+ * actuator给我们提供了一个/refresh接口，修改完git仓库信息之后，向这个接口发送一个POST信息，
+ * 就会更新配置信息了。
+ */
+@RefreshScope
 public class SpringCloudConfigClientController {
 
     @Value("${name}")
